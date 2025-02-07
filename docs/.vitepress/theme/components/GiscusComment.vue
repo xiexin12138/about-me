46460fa0fb5154ef44c136057c62a103e7ddb7ec
xiexin12138 <xiexin344@163.com>
2025-02-07 18:16:05 +0800

    加一个评论系统

---
+++ docs/.vitepress/theme/components/GiscusComment.vue
@@ -0,0 +1,27 @@
+<template>
+  <div style="margin-top: 24px">
+    <Giscus
+      id="comments"
+      repo="xiexin12138/about-me"
+      repoid="R_kgDOLyaSCg"
+      category="General"
+      categoryid="DIC_kwDOLyaSCs4CmvhT"
+      mapping="pathname"
+      term="Welcome to giscus!"
+      reactionsenabled="1"
+      emitmetadata="0"
+      inputposition="top"
+      loading="lazy"
+      :theme="isDark ? 'dark' : 'light'"
+      :key="route.path"
+    ></Giscus>
+  </div>
+</template>
+
+<script setup>
+import Giscus from "@giscus/vue";
+import { useRoute, useData } from "vitepress";
+
+const route = useRoute();
+const { isDark } = useData();
+</script>
